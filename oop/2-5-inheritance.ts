22c05bce5d2bbe860e430ea45023db2b26dfbb58
mellome-O <dhtjdal92@naver.com>
2021-03-22 12:37:59 +0900

    study inheritance start

--- oop/2-5-inheritance.ts
+++ oop/2-5-inheritance.ts
@@ -9,13 +9,60 @@
         makeCoffee(shots: number): CoffeeCup;
     }
 
-    interface CommercialCoffeeMaker{
-        makeCoffee(shots: number): CoffeeCup;
-        fillCoffeeBeans(beans: number): void;
-        clean(): void;
+    class CaffeLatteMachine{
+        private static BEANS_GRAMM_PER_SHOT:number = 7; 
+        private coffeeBeans: number = 0; 
+        
+        private constructor(coffeeBeans: number){
+            this.fillCoffeeBeans(coffeeBeans);
+        }
+
+        static makeMachine(coffeeBeans: number): CoffeeMakerBySM{
+            return new CoffeeMakerBySM(coffeeBeans);
+        }
+
+        fillCoffeeBeans(beans: number){
+            if(beans < 0){
+                throw new Error('value for beans should be greater than 0');
+            }
+            this.coffeeBeans += beans;
+        }
+
+        clean(){
+            console.log('cleaning the machine...');
+        }
+
+        private grindBeans(shots: number){
+            console.log(`grinding beans for ${shots}`);
+            if(this.coffeeBeans < shots * CoffeeMakerBySM.BEANS_GRAMM_PER_SHOT){
+                throw new Error('Not enough coffee beans!');
+            }
+            this.coffeeBeans -= shots * CoffeeMakerBySM.BEANS_GRAMM_PER_SHOT;
+        }
+
+        private preheat(): void{
+            console.log('heating up...');
+        }
+
+        private extract(shots: number): CoffeeCup{
+            console.log(`Pulling ${shots} shots...`);
+            return{
+                shots,
+                hasMilk: false,
+            };
+        }
+
+        //인터페이스에 정의한 함수 무조건 구현해야함
+        makeCoffee(shots: number): CoffeeCup{
+            this.grindBeans(shots);
+            this.preheat();
+            
+            return this.extract(shots);
+        }
+
     }
 
-    class CoffeeMakerBySM implements CoffeeMaker, CommercialCoffeeMaker{
+    class CoffeeMakerBySM implements CoffeeMaker{
         private static BEANS_GRAMM_PER_SHOT:number = 7; 
         private coffeeBeans: number = 0; 
         
@@ -78,12 +125,7 @@
     //maker2.fillCoffeeBeans(32); //사용불가
     maker2.makeCoffee(2);
 
-    //CoffeeMakerBySM 타입으로 object 받으면 public 모두 접근 가능
-    //인터페이스로 다시 타입 제한하면 인터페이스에 정의한 것만 접근 가능
-    const maker3: CommercialCoffeeMaker = CoffeeMakerBySM.makeMachine(32);
-    maker3.fillCoffeeBeans(32);
-    maker3.makeCoffee(2);
-    maker3.clean();
+
 
     
     class AmateurUser{
@@ -94,21 +136,11 @@
         }
     }
 
-    class ProBarista{
-        constructor(private machine: CommercialCoffeeMaker){}
-        makeCoffee(){
-            const coffee = this.machine.makeCoffee(2);
-            console.log(coffee);
-            this.machine.fillCoffeeBeans(45);
-            this.machine.clean();
-
-        }
-    }
 
     //동일한 CoffeeMakerBySM object 이지만 class에 제한한 인터페이스에 따라 함수사용범위 달라짐
     const newMaker: CoffeeMakerBySM = CoffeeMakerBySM.makeMachine(32);
     const amateur = new AmateurUser(newMaker);
-    const pro =  new ProBarista(newMaker);
 
-    pro.makeCoffee();
+
+
 }
